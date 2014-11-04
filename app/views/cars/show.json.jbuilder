04a1fe01019287a023169127420876c7da9427ef
Szymon Koper <sakydpozrux@gmail.com>
2014-11-04 23:57:48 +0100

    Generated Car model

---
+++ app/views/cars/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @car, :id, :model, :year, :created_at, :updated_at
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141104225141) do
+ActiveRecord::Schema.define(version: 20141104225631) do
+
+  create_table "cars", force: true do |t|
+    t.string   "model"
+    t.integer  "year"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "drivers", force: true do |t|
     t.string   "name"
