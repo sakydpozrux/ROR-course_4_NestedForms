a007256c1269648632525340f49f75c7707c9f55
Szymon Koper <sakydpozrux@gmail.com>
2014-11-05 00:50:34 +0100

    Add cascading removal of database entries

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -2,6 +2,6 @@ class Driver < ActiveRecord::Base
   validates name, presence: true
   validates surname, presence: true
 
-  has_one :car
-  has_one :garage
+  has_one :car, dependent: :delete
+  has_one :garage, dependent: :delete
 end
